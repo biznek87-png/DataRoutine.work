769fbdfbc4dcacb6801d7841d3a09571825b43e3
biznek87-png <biznek87@gmail.com>
2026-08-01 19:23:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { chromium } = require('playwright');
+const { chromium } = require('playwright-chromium');
 const app = express();
 
 app.use(express.json({ limit: '5mb' }));
